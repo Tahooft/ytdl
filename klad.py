9b9a4524c7293ca1873218ea129702b2f60dd26e
Taco Hooft <tahooft@gmail.com>
2021-03-21 10:40:22 +0100

    Added worknotes directory with some worknote files

---
+++ klad.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+url = 'https://www.youtube.com/watch?v=wZnVQT_iEYo'
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ytdl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+    ytdl.download([url])
