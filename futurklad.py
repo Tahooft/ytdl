7c3693008ee564781eb6df854cf28e82386da546
Taco Hooft <tahooft@gmail.com>
2021-03-29 10:15:06 +0200

    Added @timeit wrapper

--- futurklad.py
+++ futurklad.py
@@ -1,11 +1,14 @@
 from concurrent.futures import ThreadPoolExecutor, wait, as_completed
 from time import sleep
 from random import randint
+from timeit import timeit as timeit
 
 
+@timeit
 def return_after_5_secs(num):
     sleep(randint(1, 5))
-    return "Return of {}".format(num)
+    print(f'Return of {num}')
+    # return True
 
 
 pool = ThreadPoolExecutor(5)
@@ -13,7 +16,7 @@ futures = []
 for x in range(5):
     futures.append(pool.submit(return_after_5_secs, x))
 
-# for x in as_completed(futures):
-#     print(x.result())
+for x in as_completed(futures):
+    print(x.result())
 
-print(wait(futures))
+# print(wait(futures))
