5bfcc87539fb06c13eaa7c00fd96feb4dc09a963
Taco Hooft <tahooft@gmail.com>
2021-04-03 17:52:39 +0200

    again

--- threader.py
+++ threader.py
@@ -11,26 +11,26 @@ logger = logging.getLogger('threader')
 
 def threader(url, urls):
 
-    logger.info('A new url: %(url)s ' % url)
+    logger.info('A new url: %s' % url)
     threads = []
     urls.append(url)
 
     while len(urls) > 0:
         url = urls.pop()
         total_urls = len(urls)
-        logger.info(f'URL added: {url}')
-        logger.info(f'Total urls: {total_urls}')
+        logger.info('URL added: %s' % url)
+        logger.info('Total urls: %d ' % total_urls)
 
         t = threading.Thread(target=dl.download1, args=[dl.ydl_opts, url])
         t.start()
         thread = t.getName()
         threads.append(thread)
         total_threads = len(threads)
-        logger.info(f'Latest thread: {thread}')
-        logger.info(f'Total threads: {total_threads}')
+        logger.info('Latest thread: %s' % thread)
+        logger.info('Total threads: %s' % total_threads)
 
     t.join()
-    logger.info(f'Done these threads: {threads}\n')
+    logger.info('Done these threads: %s\n' % threads)
 
 
 # Test
