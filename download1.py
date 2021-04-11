800641acb12dce3d57870a7505a3b9973c9e1dc9
Taco Hooft <tahooft@gmail.com>
2021-04-11 12:41:40 +0200

    Added dict feedback in progresshook download

--- download1.py
+++ download1.py
@@ -9,25 +9,48 @@ logging.config.dictConfig(config)
 logger = logging.getLogger(__name__)
 
 url = 'url init'
+feedback = {'url': url}
 
 
 def my_hook(d):
+    """ Returns progress of youtube_dl(url)
+        status
+            If status is one of "downloading", or "finished",
+            the following properties may also be present:
+            * filename: The final filename (always present)
+            * tmpfilename: The filename we're currently writing to
+            * downloaded_bytes: Bytes on disk
+            * total_bytes: Size of the whole file, None if unknown
+            * total_bytes_estimate: Guess of the eventual file size,
+                                    None if unavailable.
+            * elapsed: The number of seconds since download started.
+            * eta: The estimated time in seconds, None if unknown
+            * speed: The download speed in bytes/second, None if
+                    unknown
+            * fragment_index: The counter of the currently
+                                downloaded video fragment.
+            * fragment_count: The number of fragments (= individual
+                            files that will be merged)
+    """
+
     if d['status'] == 'error':
+        feedback['status'] = 'error'
         f = d['filename']
-        logger.error('[mh] Error while downloading %s' % f)
+        feedback['filename'] = f
+        logger.error('[mh] %s' % feedback)
         raise youtube_dl.utils.DownloadError('\n[mh] Download error raised\n')
-    elif d['status'] == 'Downloading':
-        logger.debug('[mh] Downloading: %s' % url)
-        p = d['_percent_str']
-        logger.debug('[mh] Downloaded so far: %s' % p)
+    elif d['status'] == 'downloading':
+        feedback['status'] = 'downloading'
         f = d['filename']
-        logger.debug('[mh] Filename: %s' % f)
-        e = d['_eta_str']
-        logger.debug('[mh] Estimated time left: %s' % e)
+        feedback['filename'] = f
+        # logger.debug('[mh] %s' % feedback)
     elif d['status'] == 'finished':
+        feedback['status'] = 'finished'
         f = d['filename']
-        logger.info('[mh] Status finished: %s' % f)
-        print('[mh] Download of %s finished' % f)
+        feedback['filename'] = f
+        # logger.debug('[mh] %s' % feedback)
+
+    return feedback
 
 
 ydl_opts = {
@@ -55,9 +78,11 @@ def download1(ydl_opts, url):
             logger.error('[d1]Other error at: %s' % e)
         else:
             result = '[d1] Started:  %s' % url
-            logger.debug('[d1]Started: %s' % url)
+            # logger.debug('%s' % result)
         finally:
-            return result
+            feedback['result'] = result
+            logger.debug('%s' % feedback)
+            return feedback
 
 
 # Test
@@ -69,12 +94,12 @@ if __name__ == "__main__":
     # url = 'https://www.youtube.com/watch?v=nTasT5h0LEg'   # Error test
     url = 'https://www.youtube.com/watch?v=d0FV3_i-6WU+'
 
-    download1(ydl_opts, url)
+    result = download1(ydl_opts, url)
 
-    print("Ended and all that now waiting 10 secs")
+    print("Ended, now waiting 10 secs")
     logger.debug('[d1 test] End of test')
     sleep(10)
-    print('Done')
+    print('Done:\n %s' % result)
 
 # Notes
 # except youtube_dl.utils.DownloadError:
