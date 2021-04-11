4cbfc6e5b2bfda1216d6bb8aebdd5e1ebdcec9eb
Taco Hooft <tahooft@gmail.com>
2021-04-11 16:23:20 +0200

    msg from debug to info

--- downloadx.py
+++ downloadx.py
@@ -24,16 +24,16 @@ def downloadx(url, urls):
         for url in urls:
             futures = executor.submit(dl.download1, dl.ydl_opts, url)
             futures_list.append(futures)
-            logger.debug('[x] Added futures: %s' % futures)
-            logger.debug('[x] futures_list len: %d ' % len(futures_list))
+            logger.info('[x] Added futures: %s' % futures)
+            logger.info('[x] futures_list len: %d ' % len(futures_list))
 
-        logger.debug('[x] All futures added\n')
+        logger.info('[x] All futures added\n')
 
         for future in futures_list:
             try:
                 result = future.result(timeout=20)
                 results.append(result)
-                logger.debug('[x] Result: %s ' % result)
+                logger.info('[x] Result: %s ' % result)
 
             except TimeoutError as terror:
                 logger.error('[x] Timeout error!:\n%s' % terror)
@@ -45,12 +45,12 @@ def downloadx(url, urls):
 
             else:
                 frunning = future.running()
-                logger.debug('[x] Future running: %s ' % frunning)
+                logger.info('[x] Future running: %s ' % frunning)
 
                 fdone = future.done()
-                logger.debug('[x] Future done: %s ' % fdone)
+                logger.info('[x] Future done: %s ' % fdone)
 
-                logger.debug('')
+                logger.info('')
 
     return results
 
@@ -60,9 +60,20 @@ if __name__ == "__main__":
 
     from time import sleep
 
-    url = 'https://www.youtube.com/watch?v=qVpWpfD27mM'
+    url = 'https://www.youtube.com/watch?v=gm3dSYAWiUk'
 
-    urls = []
+    urls = [
+        'https://www.youtube.com/watch?v=v2r2riGruPM',
+        'https://www.youtube.com/watch?v=yvxMlQrGLkM',
+        'https://www.youtube.com/watch?v=nTasT5h0LEg',
+        'https://www.youtube.com/watch?v=7Ht9jkWXqlU',
+        'https://www.youtube.com/watch?v=84U5NlBOD64',
+        'https://www.youtube.com/watch?v=q9MAIwJMc1U',
+        'https://www.youtube.com/watch?v=ya6yw7RPjGg',
+        'https://www.youtube.com/watch?v=ALZmCy2u0jQ',
+        'https://www.youtube.com/watch?v=qVpWpfD27mM',
+        'https://www.youtube.com/watch?v=d0FV3_i-6WU+',
+        ]
 
     results = downloadx(url, urls)
     for result in results:
@@ -71,4 +82,4 @@ if __name__ == "__main__":
 
     sleep(30)
     print('Test done\n')
-    logger.debug('Test done\n')
+    logger.info('Test done\n')
