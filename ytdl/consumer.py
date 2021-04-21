1925b80e9cea296a6ea6bd593871cf51f61a700f
Taco Hooft <tahooft@gmail.com>
2021-04-21 08:38:38 +0200

    added consumer_tesrt

--- ytdl/consumer.py
+++ ytdl/consumer.py
@@ -41,38 +41,3 @@ class ConsumerThread(Thread):
         logger.info('Results: %s' % results)
         queue.join
         logger.info('Consumer stopped\n\n')
-
-
-# long_list = [
-#     'https://www.youtube.com/watch?v=qE8PG2mpo58',
-#     'https://www.youtube.com/watch?v=v2r2riGruPM',
-#     'https://www.youtube.com/watch?v=yvxMlQrGLkM',
-#     'https://www.youtube.com/watch?v=gm3dSYAWiUk',
-#     'https://www.youtube.com/watch?v=nTasT5h0LEg',
-#     'https://www.youtube.com/watch?v=7Ht9jkWXqlU',
-#     'https://www.youtube.com/watch?v=84U5NlBOD64',
-#     'https://www.youtube.com/watch?v=q9MAIwJMc1U',
-#     'https://www.youtube.com/watch?v=ya6yw7RPjGg',
-#     'https://www.youtube.com/watch?v=ALZmCy2u0jQ',
-#     'https://www.youtube.com/watch?v=qVpWpfD27mM',
-#     'https://www.youtube.com/watch?v=d0FV3_i-6WU+',
-#     'https://www.youtube.com/watch?v=2KxJ6eTY9bA',
-#     SENTINEL
-# ]
-
-short_list = [
-
-    'https://www.youtube.com/watch?v=d0FV3_i-6WU+',
-    'https://www.youtube.com/watch?v=2KxJ6eTY9bA',
-    SENTINEL
-]
-
-print(queue.qsize)
-print(SENTINEL)
-for url in short_list:
-    queue.put(url)
-
-print(queue.qsize())
-
-result = ConsumerThread().start()
-print(result)
